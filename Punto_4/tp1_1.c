d102b859a8a273718b4d51c25000f3aa2f3f6553
Geronimo Guevara <guevarageronimo61@gmail.com>
2026-04-20 15:19:04 -0300

    punto 4-e completado

--- Punto_4/tp1_1.c
+++ Punto_4/tp1_1.c
@@ -2,9 +2,17 @@
 
 int main(){
 
-    
-    printf("hola mundo");
 
+    printf("hola mundo\n");
+
+    int variable1 = 2345;
+    int *puntero1 = &variable1;
+
+    printf("el contenido del puntero es: %p\n", puntero1);
+    printf("la direccion de memoria almacenada por el puntero es: %p\n", puntero1);
+    printf("la direccion de memoria de la variable es: %p\n", &variable1);
+    printf("la dirección de memoria del puntero es : %p\n", &puntero1);
+    printf("el tamaño de memoria que utiliza la variable es: %zu", sizeof(variable1));
 
 
     return 0;
